9631a5cd5f284cb15c5eb67854df48fd921b8a3a
spiraspira <zabornik5350@gmail.com>
2024-05-06 02:04:53 +0300

    Add GreetingsSection Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import PropertyList from "./components/PropertyList";
 import Header from "./components/Header";
+import GreetingsSection from "./components/GreetingsSection";
 
 function App() {
     const myProperties = ["Квартира 1", "Квартира 2", "Дом 1", "Дом 2"];
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
         <Header />
+        <GreetingsSection />
         <PropertyList properties={myProperties} />
     </div>
   );
---
+++ src/components/GreetingsSection.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+  class GreetingsSection extends Component {
+    render() {
+      return (
+        <section className="hero-section">
+          <h1>Добро пожаловать в агентство недвижимости</h1>
+          <p>Мы поможем вам найти идеальное жилье</p>
+        </section>
+      );
+    }
+  }
+
+export default GreetingsSection;
